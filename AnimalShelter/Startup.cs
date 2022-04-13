9736cd3bf2b24e4402747bb520ffd10b3e66f7c5
Drew Henderson <drewhendersonemail@gmail.com>
2022-04-13 01:24:15 -0700

    add migrations

--- AnimalShelter/Startup.cs
+++ AnimalShelter/Startup.cs
@@ -1,16 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
+using AnimalShelter.Models;
 
 namespace AnimalShelter
 {
@@ -18,14 +13,14 @@ namespace AnimalShelter
   {
     public Startup(IConfiguration configuration)
     {
-      Configuration = configuration;
+        Configuration = configuration;
     }
 
     public IConfiguration Configuration { get; }
-
     public void ConfigureServices(IServiceCollection services)
     {
-
+      services.AddDbContext<AnimalShelterContext>(opt =>
+        opt.UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
       services.AddControllers();
       services.AddSwaggerGen(c =>
       {
@@ -42,12 +37,12 @@ namespace AnimalShelter
         app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "AnimalShelter v1"));
       }
 
-      //app.UseHttpsRedirection();
+      // app.UseHttpsRedirection();
       app.UseRouting();
       app.UseAuthorization();
       app.UseEndpoints(endpoints =>
       {
-          endpoints.MapControllers();
+        endpoints.MapControllers();
       });
     }
   }
