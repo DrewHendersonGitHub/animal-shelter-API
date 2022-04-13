649f670909e364b52bf9462141083cf91fcc2920
Drew Henderson <drewhendersonemail@gmail.com>
2022-04-13 01:34:14 -0700

    add get and post routes for cats and dogs

---
+++ AnimalShelter/Controllers/DogsController.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using AnimalShelter.Models;
+
+namespace AnimalShelter.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class DogsController : ControllerBase
+  {
+    private readonly AnimalShelterContext _db;
+
+    public DogsController(AnimalShelterContext db)
+    {
+      _db = db;
+    }
+
+    // GET api/dogs
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Dog>>> Get()
+    {
+      return await _db.Dogs.ToListAsync();
+    }
+
+    // POST api/dogs
+    [HttpPost]
+    public async Task<ActionResult<Dog>> Post(Dog dog)
+    {
+      _db.Dogs.Add(dog);
+      await _db.SaveChangesAsync();
+      return CreatedAtAction("Post", new { id = dog.DogId }, dog);
+    }
+  }
+}
