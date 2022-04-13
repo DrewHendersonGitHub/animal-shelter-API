21ecb8121eae4338fc04032a422c617e7f16483b
Drew Henderson <drewhendersonemail@gmail.com>
2022-04-13 00:27:10 -0700

    add cat and dog models

--- AnimalShelter/Startup.cs
+++ AnimalShelter/Startup.cs
@@ -42,7 +42,7 @@ namespace AnimalShelter
         app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "AnimalShelter v1"));
       }
 
-      app.UseHttpsRedirection();
+      //app.UseHttpsRedirection();
       app.UseRouting();
       app.UseAuthorization();
       app.UseEndpoints(endpoints =>
