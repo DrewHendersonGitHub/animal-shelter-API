ab9c6ea208ebeb1100e26265f7bafd5b69fddf18
Drew Henderson <drewhendersonemail@gmail.com>
2022-04-14 02:09:43 -0700

    add 10 cats and dogs

---
+++ AnimalShelter/Migrations/20220414090714_SeedData.cs
@@ -0,0 +1,147 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AnimalShelter.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Cats",
+                columns: new[] { "CatId", "Age", "Breed", "Gender", "Name" },
+                values: new object[,]
+                {
+                    { 1, 2, "Siamese", "M", "Joe" },
+                    { 9, 2, "Cat", "F", "Cat" },
+                    { 8, 6, "Normal", "M", "Mittens" },
+                    { 7, 7, "Old", "F", "Geraldo" },
+                    { 6, 5, "Fat", "M", "Whiskers" },
+                    { 10, 4, "Loud", "F", "Meow" },
+                    { 4, 1, "Small", "M", "Frank" },
+                    { 3, 3, "Big", "F", "Bob" },
+                    { 2, 6, "Tabby", "F", "Jim" },
+                    { 5, 4, "Tall", "M", "Larry" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Dogs",
+                columns: new[] { "DogId", "Age", "Breed", "Gender", "Name" },
+                values: new object[,]
+                {
+                    { 9, 6, "Cool", "F", "Ronald" },
+                    { 1, 6, "Dalmation", "F", "Spot" },
+                    { 2, 9, "Hound", "M", "Mike" },
+                    { 3, 10, "Lab", "M", "Jerry" },
+                    { 4, 4, "Fast", "F", "Jesse" },
+                    { 5, 13, "old", "M", "Michael" },
+                    { 6, 6, "Blue", "F", "Bark" },
+                    { 7, 6, "Big", "M", "Fetch" },
+                    { 8, 1, "Small", "M", "Arf" },
+                    { 10, 1, "Dog", "M", "Dog" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Cats",
+                keyColumn: "CatId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Cats",
+                keyColumn: "CatId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Cats",
+                keyColumn: "CatId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Cats",
+                keyColumn: "CatId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Cats",
+                keyColumn: "CatId",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Cats",
+                keyColumn: "CatId",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Cats",
+                keyColumn: "CatId",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Cats",
+                keyColumn: "CatId",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Cats",
+                keyColumn: "CatId",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Cats",
+                keyColumn: "CatId",
+                keyValue: 10);
+
+            migrationBuilder.DeleteData(
+                table: "Dogs",
+                keyColumn: "DogId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Dogs",
+                keyColumn: "DogId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Dogs",
+                keyColumn: "DogId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Dogs",
+                keyColumn: "DogId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Dogs",
+                keyColumn: "DogId",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Dogs",
+                keyColumn: "DogId",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Dogs",
+                keyColumn: "DogId",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Dogs",
+                keyColumn: "DogId",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Dogs",
+                keyColumn: "DogId",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Dogs",
+                keyColumn: "DogId",
+                keyValue: 10);
+        }
+    }
+}
--- AnimalShelter/Models/AnimalShelterContext.cs
+++ AnimalShelter/Models/AnimalShelterContext.cs
@@ -9,6 +9,36 @@ namespace AnimalShelter.Models
     {
     }
 
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+      builder.Entity<Cat>()
+      .HasData(
+        new Cat { CatId = 1, Name = "Joe", Breed = "Siamese", Age = 2, Gender = "M" },
+        new Cat { CatId = 2, Name = "Jim", Breed = "Tabby", Age = 6, Gender = "F" },
+        new Cat { CatId = 3, Name = "Bob", Breed = "Big", Age = 3, Gender = "F" },
+        new Cat { CatId = 4, Name = "Frank", Breed = "Small", Age = 1, Gender = "M" },
+        new Cat { CatId = 5, Name = "Larry", Breed = "Tall", Age = 4, Gender = "M" },
+        new Cat { CatId = 6, Name = "Whiskers", Breed = "Fat", Age = 5, Gender = "M" },
+        new Cat { CatId = 7, Name = "Geraldo", Breed = "Old", Age = 7, Gender = "F" },
+        new Cat { CatId = 8, Name = "Mittens", Breed = "Normal", Age = 6, Gender = "M" },
+        new Cat { CatId = 9, Name = "Cat", Breed = "Cat", Age = 2, Gender = "F" },
+        new Cat { CatId = 10, Name = "Meow", Breed = "Loud", Age = 4, Gender = "F" }
+      );
+      builder.Entity<Dog>()
+      .HasData(
+        new Dog { DogId = 1, Name = "Spot", Breed = "Dalmation", Age = 6, Gender = "F" },
+        new Dog { DogId = 2, Name = "Mike", Breed = "Hound", Age = 9, Gender = "M" },
+        new Dog { DogId = 3, Name = "Jerry", Breed = "Lab", Age = 10, Gender = "M" },
+        new Dog { DogId = 4, Name = "Jesse", Breed = "Fast", Age = 4, Gender = "F" },
+        new Dog { DogId = 5, Name = "Michael", Breed = "old", Age = 13, Gender = "M" },
+        new Dog { DogId = 6, Name = "Bark", Breed = "Blue", Age = 6, Gender = "F" },
+        new Dog { DogId = 7, Name = "Fetch", Breed = "Big", Age = 6, Gender = "M" },
+        new Dog { DogId = 8, Name = "Arf", Breed = "Small", Age = 1, Gender = "M" },
+        new Dog { DogId = 9, Name = "Ronald", Breed = "Cool", Age = 6, Gender = "F" },
+        new Dog { DogId = 10, Name = "Dog", Breed = "Dog", Age = 1, Gender = "M" }
+      );
+    }
+
     public DbSet<Cat> Cats { get; set; }
     public DbSet<Dog> Dogs { get; set; }
   }
